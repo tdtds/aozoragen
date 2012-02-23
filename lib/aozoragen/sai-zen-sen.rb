c61455ab97295eba951040f3fb67a5aa9c367fd7
TADA Tadashi <t@tdtds.jp>
2012-02-23 18:00:22 +0900

    最前線の微調整
    	* セパレータ対応
    	* divのネストに対応
    	* pgroupのあとに空改行

--- lib/aozoragen/sai-zen-sen.rb
+++ lib/aozoragen/sai-zen-sen.rb
@@ -26,18 +26,21 @@ class SaiZenSen
 			uri = @index_uri + a.attr('href')
 			chapter = Nokogiri( open( uri, 'r:utf-8', &:read ) )
 			text = ''
-			(chapter / 'section.book-page-spread' ).each do |page|
-				page.children.each do |section|
+			(chapter / 'section.book-page-spread').each do |page|
+				page./( 'hgroup', 'div.pgroup' ).each do |section|
 					case section.name
 					when 'hgroup'
 						text << "\n　　　　　#{detag section}\n\n"
 					when 'div'
-						if section.attr( 'class' ) == 'pgroup'
+						if section.attr( 'class' ) =~ /delimiter/
+							text << '　' * 5 << '─' * 10 << "\n"
+						else
 							section.children.each do |paragraph|
 								next unless paragraph.name == 'p'
 								text << "　#{detag paragraph}\n"
 							end
 						end
+						text << "\n"
 					end
 				end
 				text << "［＃改ページ］\n"
